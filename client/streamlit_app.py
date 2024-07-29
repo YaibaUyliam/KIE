4673fa9f8c3f1cee3459737f1a185c3611d5f017
yaiba <cw_yaiba@cw.com>
2024-07-29 11:40:20 +0800

    show image in mutile rows

--- client/streamlit_app.py
+++ client/streamlit_app.py
@@ -2,20 +2,35 @@ import streamlit as st
 import requests
 import base64
 import json
+import os
 
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+
+load_dotenv()  # By default, load_dotenv doesn't override existing environment variables.
+
+
+client = MongoClient(os.environ.get("URL_DB"), serverSelectionTimeoutMS=5000)
+db = client["ai-team"]
+collection = db["classify_ocr"]
+projection = {"url": 1, "_id": 0}
+sort_order = [("_id", -1)]
+limit = 20
 
 st.set_page_config(layout="wide")
 st.title("Key Info Extraction")
 
-col1, col2, col3 = st.columns(3)
+# num_rows = st.slider("Number of rows", min_value=1, max_value=10)
 
 url = st.text_input("Nhập URL của bạn vào đây")
+bank_code = st.text_input("Nhập bank code của bạn vào đây")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
-if uploaded_file is not None or url is not None:
+
+if uploaded_file or url or bank_code:
     headers = {}
     files = {}
-    payload = {}
 
     if uploaded_file:
         files = [
@@ -28,42 +43,57 @@ if uploaded_file is not None or url is not None:
                 ),
             )
         ]
+        order_list = [{}]
+
+    elif url:
+        headers = {"Content-Type": "application/json"}
+        order_list = [{"url": url}]
 
     else:
         headers = {"Content-Type": "application/json"}
-        payload = json.dumps({"url": url})
-    # b64_data = base64.b64encode(bytes_data).decode("utf-8")
-    # image_data = f"data:image/jpeg;base64,{b64_data}"
-    # with col1:
-    #     st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+
+        query = {"bank_code": bank_code.strip()}
+        order_list = list(
+            collection.find(query, projection).sort(sort_order).limit(limit)
+        )
 
     if st.button("Process Image"):
-        # if uploaded_file:
-        response = requests.request(
-            "POST",
-            "http://127.0.0.1:5003/ser_re_visual",
-            headers=headers,
-            data=payload,
-            files=files,
-        )
-        # else:
-        #     response = requests.request(
-        #         "POST", url, headers=headers, data=payload, files=files
-        #     )
+        for payload in order_list:
+            col1, col2, col3 = st.columns(3)
 
-        if response.status_code == 200:
-            data = response.json()
-            result_1_b64 = data["img_ser"]
-            result_2_b64 = data["img_re"]
-            result_3 = data["img_ser_post"]
-            result_1 = base64.b64decode(result_1_b64.split(",")[1])
-            result_2 = base64.b64decode(result_2_b64.split(",")[1])
+            payload = json.dumps(payload)
 
-            with col1:
-                st.image(result_2, caption="Result RE", use_column_width=True)
-            with col2:
-                st.image(result_1, caption="Result SER", use_column_width=True)
-            with col3:
-                st.write(result_3)
-        else:
-            st.error("Failed to process image")
+            response = requests.request(
+                "POST",
+                "http://172.19.30.25:5003/ser_re_visual",
+                headers=headers,
+                data=payload,
+                files=files,
+            )
+
+            if response.status_code == 200:
+                data = response.json()
+
+                result_1_b64 = data["img_ser"]
+                result_2_b64 = data["img_re"]
+                result_3 = data["img_ser_post"]
+
+                result_1, result_2 = None, None
+                if result_1_b64:
+                    result_1 = base64.b64decode(result_1_b64.split(",")[1])
+                if result_2_b64:
+                    result_2 = base64.b64decode(result_2_b64.split(",")[1])
+
+                with st.container():
+                    with col1:
+                        if result_2:
+                            st.image(result_2, caption="Result RE", use_column_width=True)  # fmt: skip
+                        else:
+                            pass
+                    with col2:
+                        st.image(result_1, caption="Result SER", use_column_width=True)
+                    with col3:
+                        st.write(result_3)
+
+            else:
+                st.error("Failed to process image")
